32cb52b61d3105ff9ba22204ded5f9693b08a27b
Danilo Ricardo <danilovitoriastf@gmail.com>
2025-01-03 10:51:42 -0300

    Adicionado Rota principal e slide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { HeaderComponent } from './components/header/header.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PoModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PoToolbarModule } from '@po-ui/ng-components';
+import { PoNavbarIconAction, PoNavbarItem } from '@po-ui/ng-components';
+import { PoModalModule } from '@po-ui/ng-components';
+@Component({
+  selector: 'app-header',
+  styleUrls: ['./header.component.css'],
+  templateUrl: './header.component.html',
+
+})
+export class HeaderComponent {
+
+}
